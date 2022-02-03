fc34374660cbf5dca9bc85f76a58309d0d0b4bb1
gjals <hhuurr1004@naver.com>
2022-02-03 23:52:29 +0900

    why,,,,,?

--- learn-sequelize/test/index2.js
+++ learn-sequelize/test/index2.js
@@ -3,7 +3,8 @@ var app = express();
 var PORT = 3000;
 
 // View engine setup
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //User.html이 아닌 User.ejs를 참고하게 됨
+// 따로 설치가 필요했음. npm install ejs
 
 // With middleware
 app.use('/', function(req, res, next){
@@ -13,7 +14,17 @@ app.use('/', function(req, res, next){
 
 app.get('/', function(req, res){
     console.log("Render Working")
-    res.send();
+    //res.send();
+});
+
+app.get('/comment', (req, res) => {
+    res.send('get 실행');
+    console.log('get comment 실행');
+});
+
+app.use('/comment', (req, res, next) => {
+    console.log('use 실행');
+    next();
 });
 
 app.listen(PORT, function(err){
