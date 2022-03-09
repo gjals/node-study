7f81cf0d7dd841a45f9f8d8cfc8a3b1c0167b11f
gjals <hhuurr1004@naver.com>
2022-03-10 00:33:41 +0900

    Apply .gitignore

--- project_book/routes/page.js
+++ project_book/routes/page.js
@@ -1,16 +1,11 @@
 const express= require('express');
 const router= express.Router();
-const { Post, User, Hashtag }= require('../models')
+const { Post, User, Book }= require('../models')
 const { isLogin, isNotLogin }= require('./middlewares');
 const db= require('../models/index');
-const { hash } = require('bcrypt');
-const { promise } = require('bcrypt/promises');
 
 router.use((req, res, next) => {
     res.locals.user= req.user;
-    res.locals.followerCount= req.user? req.user.Followers.length : 0;
-    res.locals.followingCount= req.user? req.user.Followings.length : 0;
-    res.locals.followerIDList= req.user? req.user.Followings.map(f=>f.id) : [];
     next();
 });
 
@@ -19,36 +14,28 @@ router.get('/profile', isLogin, (req, res) => {
 });
 
 router.get('/join', isNotLogin, (req, res) => {
-    console.log('join get됨');
     res.render('join');
-    console.log('join get end');
+});
+
+router.get('/login', (req, res=>{
+    res.render('login');
+}));
+
+router.get('/post', (req, res)=>{
+    res.render('login');
 });
 
 router.get('/', async (req, res, next) => {
     try {
-        const UserPost= db.sequelize.models.UserPost;
         const posts= await Post.findAll({
             include: [{
-                model: User, //그래서 post.User 이렇게 쓰는 건가?
-                attributes: ['id', 'nick'],
-            },{
-                model:UserPost,
+                model: Book,
+            }, {
+                model: User,
             }],
             order:[[ 'createdAt', 'DESC']],
         });
-        console.log(req.user.id);
-        await Promise.all(
-            posts.map(async(list,idx)=>{
-                const result= await UserPost.findOne({
-                    where:{
-                        UserId:req.user.id,
-                        PostId:list.id,
-                    }
-                });
-                if(result) posts[idx]['dataValues'].good=true;
-                else posts[idx]['dataValues'].good=false;
-            })
-        );
+        
         console.log(posts);
         console.log('get / render before');
         res.render('main', { posts });
@@ -56,28 +43,6 @@ router.get('/', async (req, res, next) => {
     } catch (err) {
         console.log(err);
         next(err);
-    }
-});
-
-
-router.get('/hashtag', async (req, res, next)=>{
-    console.log('hashtag get start');
-    const query= req.query.post_search_text; //req.query랑 req.body랑 다른 점?
-    if(!query) {
-        return res.redirect('/');
-    }
-    try {
-        const hashtag= await Hashtag.findOne({ where: { title: query }});
-        let posts= [];
-        
-        if(hashtag) {
-            posts= await hashtag.getPosts({ include: [{ model: User }] });
-        }
-        console.log('hashtag get end!');
-        return res.render('main', { posts });
-    } catch (err) {
-        console.log(err);
-        return next(err);
     }
 });
 
--- project_book/routes/user.js
+++ project_book/routes/user.js
@@ -1,54 +1,7 @@
 const express= require('express');
 const { isLogin }= require('./middlewares');
-
-const {User,Post,UserPost}=require('../models');
-
-
+const {User}=require('../models');
 const router= express.Router();
-const db= require('../models/index');
-
-//axios.post(`/user/${userid}/follow`)
-router.post('/:id/follow', isLogin, async (req, res, next)=>{
-    try {
-        console.log('user follow 도착', req.user.id, '가 ', req.params.userid, '를 팔로우함');
-        const user= await User.findOne({ where: { id: req.user.id }});
-        if(user) {
-            console.log('user있음에 들어옴!');
-            await user.addFollowing(parseInt(req.params.id, 10));
-            res.send('success'); //이게 어디에 나올까?
-            console.log('user 렌더끝');
-        } else {
-            console.log('not user');
-        }
-    } catch (err) {
-        console.log(err);
-        next(err);
-    }
-});
-
-router.post('/:id/unfollow', isLogin, async (req, res, next)=>{
-    try {
-        console.log('user unfollow에 옴');
-        console.log('asdf', req.params.id, 'asdf', req.user.id);
-        console.log(parseInt(req.params.id, 10));
-        const user= await User.findOne({ where: { id: req.user.id }});
-        if(user) {
-            const Follow= db.sequelize.models.Follow;
-            await Follow.destroy({
-                where: {
-                    followingId: parseInt(req.params.id, 10),
-                    followerId: req.user.id,
-                }
-            });
-            res.send('success');
-        } else {
-            console.log('not user');
-        }
-    } catch (err) {
-        console.log(err);
-        next(err);
-    }
-});
 
 router.post('/update', isLogin, async (req, res, next)=>{
     try {
@@ -59,42 +12,6 @@ router.post('/update', isLogin, async (req, res, next)=>{
         } else {
             console.log('not user');
         }
-    } catch (err) {
-        console.log(err);
-        next(err);
-    }
-});
-
-router.post('/good', isLogin, async (req, res, next)=>{
-    try {
-        const {id,postid}=req.body;
-        console.log(id, postid);
-        const userPostChk= await UserPost.findOne({
-            where:{
-                UserId:id,
-                PostId:postid,
-            }
-        });
-        console.log(userPostChk);
-        if(userPostChk){
-            console.log(1);
-            console.log('===============');
-            await UserPost.destroy({
-                where:{
-                    UserId:id,
-                    PostId:postid
-                }
-            })
-        }else{
-            console.log(2);
-            console.log('===============');
-            await UserPost.create({
-                UserId:id,
-                PostId:postid,
-                gg:'1',
-            })
-        }
-        return res.json({status:'true'});
     } catch (err) {
         console.log(err);
         next(err);
