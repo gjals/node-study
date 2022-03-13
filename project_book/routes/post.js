4fc94318f184dca59d8cbf717900b4f2e1edc522
gjals <hhuurr1004@naver.com>
2022-03-13 23:56:01 +0900

    Apply .gitignore

--- project_book/routes/post.js
+++ project_book/routes/post.js
@@ -5,34 +5,14 @@ const router= express.Router();
 const convert = require('xml-js');
 const request = require('request');
 
-router.post('/newPost', isLogin, async (req, res, next) => {
-    //try {
-    //     console.log(req.body.url);
-    //     console.log(req.body.post_image_url);
-    //     console.log(req.body.post_text);
-    //     const post= await Post.create({
-    //         content: req.body.post_text,
-    //         img: req.body.post_image_url,
-    //         UserId: req.user.id, // req.user은 어디서 온거지??
-    //     });
-        
-    //     const hashtags= req.body.post_text.match(/#[^\s#]+/g);
-    //     if(hashtags) {
-    //         const result= await Promise.all(//왜 굳이 프로미스를 쓰지?
-    //             hashtags.map(tag => {
-    //                 return Hashtag.findOrCreate({
-    //                     where: { title: tag.slice(1).toLowerCase() },
-    //                 })
-    //             }),
-    //         );
-    //         await post.addHashtags(result.map(r=>r[0]));
-    //     }
-    //     console.log('/post end');
-    //     res.redirect('/');
-    // } catch (err) {
-    //     console.error(err);
-    //     next(err);
-    // }
+router.post('/admin', isLogin, async (req, res, next) => {
+    try {   
+        const { title, author, kdc, isbn }= req.body;
+        console.log(title, author, kdc, isbn);
+        const posts= Post.findAll({ where: { isbn } });
+    } catch(err) {
+        next(err);
+    }
 })
 
 
