985b1be8046a4b31c368bca3647c61bd0f870797
gjals <hhuurr1004@naver.com>
2022-04-02 23:56:00 +0900

    Apply .gitignore

--- project_book/routes/user.js
+++ project_book/routes/user.js
@@ -3,14 +3,30 @@ const { isLogin }= require('./middlewares');
 const {User}=require('../models');
 const router= express.Router();
 
-router.post('/update', isLogin, async (req, res, next)=>{
+router.post('/:userid/updateNick', isLogin, async (req, res, next)=>{
     try {
-        const user= await User.findOne({ where: { id: req.user.id }});
+        const user= await User.findOne({ where: { id: req.params.userid }});
         if(user) {
-            await User.update({ nick: req.body.update_nick_text }, { where: { id: req.user.id }});
-            res.redirect('/profile');
+            await User.update({ nick: req.body.nick }, { where: { id: req.params.userid }});
+            return res.json({ code: 200, message:'잘 변경됨'});
         } else {
-            console.log('not user');
+            return res.json({ code: 500, message:'해당되는 유저 없음'});
+        }
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+});
+
+
+router.post('/:userid/updatePassword', isLogin, async (req, res, next)=>{
+    try {
+        const user= await User.findOne({ where: { id: req.params.userid }});
+        if(user) {
+            await User.update({ password: req.body.password }, { where: { id: req.params.userid }});
+            return res.json({ code: 200, message:'잘 변경됨'});
+        } else {
+            return res.json({ code: 500, message:'해당되는 유저 없음'});
         }
     } catch (err) {
         console.log(err);
