3fc81ffad8f1d35bf7db9980272b218902c07de5
gjals <hhuurr1004@naver.com>
2022-04-23 19:43:28 +0900

    Apply .gitignore

--- project_book/config/config.js
+++ project_book/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     "development": {
         "username": "root",
-        "password": process.env.SEQUELIZE_PASSWORD,
+        "password": "1234",
         "database": "projectbook",
         "host": "127.0.0.1",
         "dialect": "mysql"
